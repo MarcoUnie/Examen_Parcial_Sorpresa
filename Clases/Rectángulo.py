6c6cca0862487b6c54184325dcad619622dd0373
MarcoUnie <mmagecoyt@gmail.com>
2025-03-28 09:46:04 +0100

    metodo area

--- Clases/Rectángulo.py
+++ Clases/Rectángulo.py
@@ -5,3 +5,9 @@ class Rectangulo:
 
     def base(self):
         return abs(self.puntoi[0] - self.puntof[0])
+
+    def altura(self):
+        return abs(self.puntoi[1] - self.puntof[1])
+    
+    def area(self):
+        return self.base() * self.altura()
